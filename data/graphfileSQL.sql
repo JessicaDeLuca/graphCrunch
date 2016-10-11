827f9904965cd337f448cfa1bc4f013b41fa27ac
JessicaDeLuca <jessica.deluca1@gmail.com>
2016-10-11 16:09:09 -0700

    SQL_pairs

--- data/graphfileSQL.sql
+++ data/graphfileSQL.sql
@@ -1,4 +1,25 @@
 
+
 SELECT company, news_url, COUNT(news_url)
 FROM cb_news GROUP BY news_url, company
 HAVING (COUNT(news_url) > 1 );
+
+
+" Get list of common articles for 2 companies"
+   SELECT a.company, b.company, a.title, a.uuid, a.news_url, a.posted_on
+   FROM holddups as a
+   INNER JOIN holddups as b
+   ON a.uuid=b.uuid
+  WHERE a.company='Netflix' AND b.company = 'Google'
+
+"Count of edges for each pair of companies"
+  SELECT a.company, b.company, count(a.uuid)
+  FROM holddups as a
+  INNER JOIN holddups as b
+  ON a.uuid=b.uuid
+  GROUP BY a.company, b.company
+  HAVING a.company='Netflix' AND b.company = 'Google'
+
+  SELECT a.company, b.company, count(a.uuid) FROM holddups as a INNER JOIN holddups as b ON a.uuid=b.uuid GROUP BY a.company, b.company LIMIT 10
+
+  
